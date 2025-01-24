7bcd009bf0b0a8d013843e059b262f0712481680
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-01-24 17:26:00 +0100

    Base del proyecyo creada

---
+++ app/src/main/java/com/example/pr06_retrofit_adrian_tiago/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pr06_retrofit_adrian_tiago
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pr06_retrofit_adrian_tiago.ui.theme.PR06_Retrofit_Adrian_TiagoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PR06_Retrofit_Adrian_TiagoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PR06_Retrofit_Adrian_TiagoTheme {
+        Greeting("Android")
+    }
+}
