6d1ec62fe16aee8df20f1a4537d83d5aeef08498
Adrian <adrianortegaolgoso@gmail.com>
2025-01-31 16:33:23 +0100

    feat: terminado el viewModel

---
+++ app/src/main/java/com/example/pr06_retrofit_adrian_tiago/viewmodel/ViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.pr06_retrofit_adrian_tiago.viewmodel
+
+import com.example.pr06_retrofit_adrian_tiago.api.Repository
+import com.example.pr06_retrofit_adrian_tiago.model.DatosAPI
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class ViewModel: ViewModel() {
+
+    private val repository = Repository()
+    private val _loading = MutableLiveData(true)
+    val loading = _loading
+    private val _games = MutableLiveData<DatosAPI>()
+    val games = _games
+
+    fun getGames() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = repository.getAllGames()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    _games.value = response.body()
+                    _loading.value = false
+                } else {
+                    Log.e("Error :", response.message())
+                }
+            }
+        }
+    }
+}
