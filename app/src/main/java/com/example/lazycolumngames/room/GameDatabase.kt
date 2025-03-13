5928dd1009149a73000b1c0c6cc2e82d2c1607d4
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-13 18:22:09 +0100

    feat: "gameDatabase creado"

---
+++ app/src/main/java/com/example/lazycolumngames/room/GameDatabase.kt
@@ -0,0 +1,8 @@
+package com.example.lazycolumngames.room
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.lazycolumngames.model.Juego
+
+class GameDatabase {
+}
