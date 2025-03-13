dff982483a119f75c4a336b9c75a9ac1027961cc
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-13 18:17:55 +0100

    feat: "rutas de gameDao añadidas"

--- app/src/main/java/com/example/lazycolumngames/room/GameDao.kt
+++ app/src/main/java/com/example/lazycolumngames/room/GameDao.kt
@@ -2,7 +2,23 @@ package com.example.lazycolumngames.room
 import androidx.room. Delete
 import androidx.room. Insert
 import androidx.room. Query
+import com.example.lazycolumngames.model.DatosAPI
+import com.example.lazycolumngames.model.Juego
 
-class GameDao {
+interface GameDao {
+    @Query("SELECT * FROM games WHERE is_favourite = 1")
+    fun getFavourite() : MutableList<Juego>
+
+    @Query("SELECT * FROM games WHERE title = :title ")
+    fun findByTitle (title: String)
+
+    @Query("SELECT is_favourite FROM games WHERE title = :title ")
+    fun isFavourite (title: String)
+
+    @Insert
+    fun likeGame(game: Juego)
+
+    @Delete
+    fun dislikeGame(game: Juego)
 
 }
