689370eb30b49b9375778be57e0370937b7b4a58
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-13 18:33:12 +0100

    feat: "funcion para crear una base de datos llamada GameDatabase creada"

--- app/src/main/java/com/example/lazycolumngames/room/GameApplication.kt
+++ app/src/main/java/com/example/lazycolumngames/room/GameApplication.kt
@@ -4,5 +4,12 @@ import android.app.Application
 import androidx.room.Room
 
 class GameApplication {
+    companion object {
+        lateinit var database: GameDatabase
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        database = Room.databaseBuilder(this, GameDatabase::class.java, "GameDatabase").build()
+    }
 }
